e614aca4839683bee88f4260b6717bd6f8c58afb
Peter Paprotny <pappet@web.de>
2025-04-21 23:52:34 +0200

    Remove all __pycache__ directories from tracking

--- core/events/event_types.py
+++
@@ -1,9 +0,0 @@
-# core/event_types.py
-from enum import Enum, auto
-
-class EventType(Enum):
-    """
-    Enum for different types of events in the game.
-    """
-    ENERGY_CHANGED = auto()
-    HEALTH_CHANGED = auto()
--- tests/conftest.py
+++
@@ -1,4 +0,0 @@
-# tests/conftest.py
-import os, sys
-# Projekt‑Root (eine Ebene über tests/) zum Pfad hinzufügen
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
